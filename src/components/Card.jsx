e5ffef7a0a4f27320b61a08b0e1380a1dc5cceb3
Albs1de <dkoskovi@stud.hs-offenburg.de>
2023-11-27 11:17:33 +0100

    Button für die Cards hinzugefügt

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,12 +6,12 @@ const Card = ({id, title, price, description, category, image, rating}) => {
   
     <div id={id} className="card-container bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 m-4">
       <img src={image} alt={title} className="w-32 h-32" />
-      <h3 className="text-lg font-bold text-gray-800 px-4 pt-4">{title}</h3>
-      <p>Preis: {price}€</p>
-      <p>Kategorie: {category}</p>
+      <h3 className="text-lg font-bold text-blue px-4 pt-4">{title}</h3>
+      <p>Price: {price}€</p>
+      <p>Category: {category}</p>
       <p>{description}</p>
       <p>Rating : {rating.rate}</p>
-
+      <button className="bg-blue-700 text-white font-bold hover:bg-white hover:text-blue-700 hover:border py-2 px-4 rounded m-4">Buy</button>
 
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+// import {Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <ul className="flex space-x-4">ok</ul>
+    <nav className="bg-gray-800 text-white p-4 flex justify-between ">
+     <ul className="flex">
+      <li className="ml-4 hover:text-gray-300">Home</li>
+      <li className="ml-4 hover:text-gray-300">MyList</li>
+      <li className="ml-4 hover:text-gray-300">Shopping cart</li>
+     </ul>
+
     </nav>
   )
 }
