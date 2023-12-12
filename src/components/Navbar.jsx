56b61d6bbb0cacb0d22a2ce1ba54b286da56e8a0
Albs1de <dkoskovi@stud.hs-offenburg.de>
2023-12-12 19:24:49 +0100

    Fix: Warenkorb umgewandlet mit ul und li und optisch verbessert

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,33 +26,32 @@ const Navbar = () => {
         >
           Shopping Cart
           {isCartOpen && (
-            <div className=" bg-white text-black rounded-md absolute right-0 w-full">
-              {/*TODO wenn gleiches Element, dann nur 1x anzeigen und Anzahl aufzählen */}
+            <div className="bg-white text-black rounded-md absolute right-0 w-full">
               {cartList.length > 0 ? (
-                cartList.map((item) => (
-                  <div
-                    key={item.id}
-                    className=" flex items-center px-4 py-2 m-2"
-                  >
-                    <img src={item.image} alt={item.title} className="w-10" />
-                    <p className="ml-4 text-sm">{item.title}</p>
-                    <p className="ml-24 font-bold">{item.price}€</p>
-                    {/*TODO! Icon einfügen zum löschen */}
-                    <button
-                      className="btn "
-                      onClick={() => deleteFromCart(item)}
+                <ul role="list" className="p-2 divide-y divide-slate-200">
+                  {cartList.map((item) => (
+                    <li
+                      key={item.id}
+                      className="flex items-center px-4 py-2 m-2"
                     >
-                      Delete
-                    </button>
-                  </div>
-                ))
+                      <img src={item.image} alt={item.title} className="w-10" />
+                      <p className="ml-4 text-sm">{item.title}</p>
+                      <p className="ml-auto font-bold">{item.price}€</p>
+                      <button
+                        className="btn ml-4"
+                        onClick={() => deleteFromCart(item)}
+                      >
+                        Delete
+                      </button>
+                    </li>
+                  ))}
+                </ul>
               ) : (
                 <div className="px-4 py-2 text-gray-700">
-                  {/*TODO Icon hinzufügen */}
                   Dein Warenkorb ist leer.
                 </div>
               )}
-              <div className="bg-black text-white">
+              <div className="bg-black text-white p-2">
                 <p>Gesamtpreis: {totalPrice}€</p>
               </div>
             </div>
